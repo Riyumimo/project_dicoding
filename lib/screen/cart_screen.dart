bf91d290cc6cd4bee41eb0d6b9a3a92d6a92c3dd
Ryumimo <ilhamsuherman80@gmail.com>
2022-06-24 23:13:05 +0700

    sama

--- lib/screen/cart_screen.dart
+++ lib/screen/cart_screen.dart
@@ -6,19 +6,69 @@ class CartScreen extends StatelessWidget {
   static Route route() {
     return MaterialPageRoute(
       settings: const RouteSettings(name: routeName),
+      
       builder: (context) => const CartScreen(),
+
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    var heightscreen = MediaQuery.of(context).size.height;
+    var widthscreen = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
-        title: const Text("CartScreen"),
+        elevation: 0.0,  
+        backgroundColor: Colors.transparent,
+        title: const Text("Order Details",style: TextStyle(fontWeight: FontWeight.w600,color: Colors.black87,fontSize: 24),),
+        centerTitle:true,
       ),
-      body: const Center(
-        child: Text('Ini CartScreen'),
-      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(height: 10,),
+            Text("My Cart" ,style: TextStyle(fontWeight: FontWeight.w500,color: Colors.black87,fontSize: 22),),
+            SizedBox(height: 10,),
+            Container(
+              width: widthscreen,
+             height: heightscreen*0.18, 
+             color: Colors.amber,
+             child: Row(
+               children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      width: widthscreen*0.30,
+                      color: Colors.red,
+                    ),
+                    
+                  ),  
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(9),
+                  child: SizedBox(
+                    
+                    // width: heightscreen*0.80,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("data"),
+                        Text("data"),
+                        Text("data"),
+                      ],
+                      
+                    ),
+                  ),
+                )
+               ],
+             ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
